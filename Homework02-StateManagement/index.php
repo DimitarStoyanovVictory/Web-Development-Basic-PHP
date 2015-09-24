8d68b5db7ff9929032908ffe0b194c5996d85880
DimitarStoyanovVictory <Dimitar.Stoyanov.Victory@gmail.com>
2015-09-24 13:14:33 +0300

    change
    change

--- Homework02-StateManagement/index.php
+++ Homework02-StateManagement/index.php
@@ -2,4 +2,6 @@
     <h1>Welcome pleace enter your name! (first name of full name)</h1>
     <input type="text" name="indexName">
     <input type="submit" value="Start Game">
+    <p>!You can't enter numbers for name</p>
 </form>
+
--- Homework02-StateManagement/play.php
+++ Homework02-StateManagement/play.php
@@ -1,49 +1,60 @@
-<!--<script type='text/javascript'>-->
-<!--    function showButton(){-->
-<!--        document.getElementById("hiddenPlayButton").style.display = 'block';-->
-<!--    }-->
-<!--</script>-->
-
 <?php
 session_start();
-if (!isset($_POST['indexName'])) {
-    header('Location: index.php');
-    exit;
+if(!isset($_SESSION['name'])) {
+    if (!isset($_POST['indexName'])) {
+        header('Location: index.php');
+        exit;
+    }
+
+    $randomNumber = rand(1, 100);
+    $_SESSION['name'] = $_POST['indexName'];
+    $_SESSION['number'] = $randomNumber;
+}
+?>
+
+<?php
+if(isset($_POST['indexName']))
+{
+    $isValidName = strcspn((trim($_POST['indexName'])), '0123456789');
+    if (strlen($_SESSION['name']) != $isValidName){
+        header('Location: index.php');
+        exit;
+    }
 }
 ?>
 
 <form action="" method="POST">
-    <h1>Hello pleace enter number in range [1...100]</h1>
+    <h1>Hello <?= $_SESSION['name']?> pleace enter number in range [1...100]</h1>
     <input type="text" name="number">
-    <input type="hidden" name="name" value="<?= $_POST['indexName'];?>"/>
     <input type="submit" name="submitNumber" value="Submit number">
-    <input id="hiddenPlayButton" type="hidden" name="playAgain" value="Play again" onlick="ShowButton()">
 </form>
 
-<?php
+<?php if (isset($_POST['submitNumber'])): ?>
+    <?php $playerNum = (int)$_POST['number']; ?>
 
-if (isset($_POST['submitNumber'])){
-    $count = 0;
-    $count++;
+    <?php if(!is_int($playerNum) || empty($playerNum)): ?>
+        <p>Input can't be string pleace enter number!</p>
+    <?php else: ?>
+        <?php if($playerNum < 1 || $playerNum > 100): ?>
+            <p>Invalid number! Pleace enter number in the given limits!</p>
+        <?php endif; ?>
 
-    if ($count == 1) {
-    $specialNumber = rand(1, 100);
-    var_dump($specialNumber);
-    var_dump($count);
-    }
+        <?php if($playerNum === $_SESSION['number']): ?>
+            <p>CONGRATULATIONS, <?=htmlspecialchars($_SESSION['name']); ?>!</p>
+            <a href="index.php">[Play again?]</a>
+            <?php session_destroy(); ?>
+        <?php endif; ?>
 
-    if ($specialNumber == $_SESSION['submitNumber']) {
-    echo 'Congratulations, ' . $_POST['name'];
-    } else if ($specialNumber < 1 || $specialNumber > 100) {
-    echo 'InvalidNumber';
-    } else if ($specialNumber < $_SESSION['number']) {
-    echo 'Up';
-    } else if ($specialNumber > $_SESSION['number']) {
-    echo 'Down';
-    } else {
-    echo 'Invalid format pleace enter a integer nubmer';
-    }
-};
-?>
+        <?php if($playerNum < 1 || $playerNum > 100): ?>
+            <p>Input can't be string pleace enter number</p>
+        <?php endif; ?>
 
+        <?php if($playerNum > $_SESSION['number']): ?>
+            <p><b>DOWN! Enter new number.</b></p>
+        <?php endif; ?>
 
+        <?php if($playerNum < $_SESSION['number']): ?>
+            <p><b>UP! Enter new number.</b></p>
+        <?php endif;?>
+    <?php endif;?>
+<?php endif; ?>
